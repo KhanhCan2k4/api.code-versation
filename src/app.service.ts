4fe5999ac618712b23670e87756f450a6a4fbd5b
khanhcan2k4 <levietkhanh2k4@gmail.com>
2025-03-30 23:33:59 +0700

    add beautifull logs into app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { blue, gray, green, magenta, red, yellow } from 'colorette';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Welcome to Code-versation API!';
+  }
+
+  private static log(
+    level: string,
+    colorFn: (msg: string) => string,
+    message: string,
+  ) {
+    const timestamp = gray(`[${new Date().toISOString()}]`);
+    console.log(`${timestamp} ${colorFn(`[${level}]`)} ${message}`);
+  }
+
+  static info(message: string) {
+    this.log('INFO', blue, message);
+  }
+
+  static warn(message: string) {
+    this.log('WARN', yellow, message);
+  }
+
+  static error(message: string) {
+    this.log('ERROR', red, message);
+  }
+
+  static debug(message: string) {
+    this.log('DEBUG', magenta, message);
+  }
+
+  static success(message: string) {
+    this.log('SUCCESS', green, message);
   }
 }
