7387409285dde02bbe72c2a0c3657592d02d930a
khanhcan2k4 <levietkhanh2k4@gmail.com>
2025-07-23 12:41:56 +0700

    change app's setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,68 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { CharacterModule } from './modules/CharacterModule';
-import { AccountModule } from './modules/AccountModule';
-import { AIAssistantModule } from './modules/AIAssistantModule';
-import { AIModelModule } from './modules/AiModelModule';
-import { ConversationModule } from './modules/ConversationModule';
-import { LanguageModule } from './modules/LanguageModule';
-import { LearningSentenceModule } from './modules/LearningSentenceModule';
-import { PromptModule } from './modules/PromptModule';
-import { ReportModule } from './modules/ReportModule';
-import { ReportReasonModule } from './modules/ReportReasonModule';
-import { RewardModule } from './modules/RewardModule';
-import { SentenceModule } from './modules/SentenceModule';
-import { SituationModule } from './modules/SituationModule';
-import { StreakModule } from './modules/StreakModule';
-import { TeamModule } from './modules/TeamModule';
-import { TokenModule } from './modules/TokenModule';
-import { VoiceModule } from './modules/VoiceModule';
 import * as dotenv from 'dotenv';
 import {
   ServeStaticModule,
   ServeStaticModuleOptions,
 } from '@nestjs/serve-static';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
-import { MailModule } from './modules/MailModule';
+import { Language } from './models/Language';
+import { LanguageController } from './controllers/LanguageController';
+import { LanguageService } from './services/LanguageService';
+import { TeamRole } from './models/TeamRole';
+import { TeamRoleController } from './controllers/TeamRoleController';
+import { TeamRoleService } from './services/TeamRoleService';
+import { Account } from './models/Account';
+import { Voice } from './models/Voice';
+import { AccountController } from './controllers/AccountController';
+import { AccountService } from './services/AccountService';
+import { VoiceController } from './controllers/VoiceController';
+import { VoiceService } from './services/VoiceService';
+import { Situation } from './models/Situation';
+import { TopicController } from './controllers/TopicController';
+import { SituationService } from './services/SituationService';
+import { MailerModule, MailerOptions } from '@nestjs-modules/mailer';
+import { Token } from './models/Token';
+import { TokenService } from './services/TokenService';
+import { OTP } from './models/OTP';
+import { OTPService } from './services/OTPService';
+import { Conversation } from './models/Conversation';
+import { Topic } from './models/Topic';
+import { ConversationService } from './services/ConversationService';
+import { ConversationController } from './controllers/ConversationController';
+import { LineOfSpeech } from './models/LineOfSpeech';
+import { Practice } from './models/Practice';
+import { Question } from './models/Question';
+import { PracticeController } from './controllers/PracticeController';
+import { PracticeService } from './services/PracticeService';
+import { Prompt } from './models/Prompt';
+import { PromptController } from './controllers/PromptController';
+import { PromptService } from './services/PromptService';
+import { TopicService } from './services/TopicService';
+import { ImageService } from './services/ImageService';
+import { Log } from './models/Log';
 
 dotenv.config();
+
+const models = [
+  Language,
+  TeamRole,
+  Account,
+  Voice,
+  Situation,
+  Token,
+  OTP,
+  Conversation,
+  Topic,
+  LineOfSpeech,
+  Practice,
+  Question,
+  Prompt,
+  Log,
+];
 
 const typeOrmModuleOptions: TypeOrmModuleOptions = {
   type: 'mysql',
@@ -36,39 +71,65 @@ const typeOrmModuleOptions: TypeOrmModuleOptions = {
   username: process.env.MYSQL_USERNAME || 'root',
   password: process.env.MYSQL_PASSWORD || '',
   database: process.env.MYSQL_DATABASE || 'code-versations',
-  entities: [__dirname + '/src/models'],
+  entities: models,
   synchronize: true, // Disable in production
 };
 
 const serveStaticModuleOptions: ServeStaticModuleOptions = {
-  rootPath: join(__dirname, 'public/images'), // Serve "datas" folder
+  rootPath: join(__dirname, '..', 'public/images'), // Serve "datas" folder
   serveRoot: '/images', // URL path prefix
+};
+
+const mailerOptions: MailerOptions = {
+  transport: {
+    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+    port: +(process.env.SMTP_PORT || 587),
+    auth: {
+      user: process.env.SMTP_USER || '22211tt2577@mail.tdc.edu.vn',
+      pass: process.env.SMTP_PASS || '',
+    },
+  },
+  template: {
+    dir: join(__dirname, '..', 'public', 'views', 'mails'),
+    adapter: new HandlebarsAdapter(),
+    options: {
+      strict: true,
+    },
+  },
 };
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmModuleOptions),
+    TypeOrmModule.forFeature(models),
     ServeStaticModule.forRoot(serveStaticModuleOptions),
-    AccountModule,
-    AIAssistantModule,
-    AIModelModule,
-    CharacterModule,
-    ConversationModule,
-    LanguageModule,
-    LearningSentenceModule,
-    PromptModule,
-    ReportModule,
-    ReportReasonModule,
-    RewardModule,
-    SentenceModule,
-    SituationModule,
-    StreakModule,
-    TeamModule,
-    TokenModule,
-    VoiceModule,
-    MailModule,
+    MailerModule.forRoot(mailerOptions),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    LanguageController,
+    TeamRoleController,
+    AccountController,
+    VoiceController,
+    TopicController,
+    ConversationController,
+    PracticeController,
+    PromptController,
+  ],
+  providers: [
+    AppService,
+    LanguageService,
+    TeamRoleService,
+    AccountService,
+    VoiceService,
+    SituationService,
+    TokenService,
+    OTPService,
+    ConversationService,
+    PracticeService,
+    PromptService,
+    TopicService,
+    ImageService,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,22 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3000;
 
+  // Allow only specific origin
+  const allowedOrigins = require('../public/jsons/origins.json');
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
+
   await app.listen(port, () => {
-    AppService.info(`listening on port ${port}`);
+    AppService.info(`listening on port ${port}`, {}, false);
+    AppService.info('allowed Origins ', allowedOrigins, false);
   });
 }
-bootstrap();   
+bootstrap();
